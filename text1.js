be2f5623868220cb78e7326260cce93bfe8e7760
debs-DS <deborahtunde9@gmail.com>
2025-03-16 10:03:36 +0000

    27

--- text1.js
+++ text1.js
@@ -3,3 +3,6 @@ function black(x) {x.style.color="black"}
 
 function fade(x) {x.style.backgroundColor="grey", x.style.color="white"}
 function button(x) {x.style.backgroundColor="rebeccapurple", x.style.color="black"}
+
+function big(x) {x.style.width="350px"}
+function small(x) {x.style.width="300px"}
